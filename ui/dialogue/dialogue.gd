6d71ffdadcdc66e72bf2981770dac746dd4affe2
Isaac Ng Jun Jie <143304834+IzN432@users.noreply.github.com>
2026-06-25 18:41:45 +0800

    Update dialogue to have two speakers (#24)

--- ui/dialogue/dialogue.gd
+++ ui/dialogue/dialogue.gd
@@ -31,6 +31,37 @@ var allowed_to_continue = false
 var allowed_to_skip_finish = false
 var should_skip_finish = false
 
+@onready var ap_1: AnimationPlayer = $speaker_1_ap
+@onready var ap_2: AnimationPlayer = $speaker_2_ap
+
+func enable(sb: SpeakerBox):
+	sb.sprite.show()
+	sb.text.get_parent().get_parent().show()
+func disable(sb: SpeakerBox):
+	sb.sprite.hide()
+	sb.text.get_parent().get_parent().hide()
+
+var waiting_on_animation: Array[bool] = [ false, false ]
+
+func play_animation(is_left: bool, is_entering: bool):
+	waiting_on_animation[0 if is_left else 1] = true
+	(ap_1 if is_left else ap_2).animation_finished.connect(
+		func(_x): on_finish_animation(is_left), CONNECT_ONE_SHOT)
+	
+	if is_entering:
+		(ap_1 if is_left else ap_2) \
+			.play("dialogue_enter_one" if is_left else "dialogue_enter_two")
+	else:
+		(ap_1 if is_left else ap_2) \
+			.play_backwards("dialogue_enter_one" if is_left else "dialogue_enter_two")
+
+func wait_for_animations():
+	while waiting_on_animation[0] || waiting_on_animation[1]:
+		await get_tree().process_frame
+
+func on_finish_animation(is_left: bool):
+	waiting_on_animation[0 if is_left else 1] = false
+
 func display_dialogue_chain(dialogue_chain: DialogueChainData):
 	for dialogue in dialogue_chain.dialogues:
 		display_dialogue(dialogue)
@@ -38,6 +69,11 @@ func display_dialogue_chain(dialogue_chain: DialogueChainData):
 		if not success:
 			on_dialogue_chain_end.emit(false)
 			return
+	if (displaying_left != null):
+		play_animation(true, false)
+	if (displaying_right != null):
+		play_animation(false, false)
+	await wait_for_animations()
 	on_dialogue_chain_end.emit(true)
 
 func unfocus(sb: SpeakerBox):
@@ -68,7 +104,30 @@ func make_invisible(sb: SpeakerBox):
 func update_speaker(sb: SpeakerBox, s: SpeakerData):
 	sb.text.text = s.display_name
 	sb.sprite.texture = s.sprite
+
+var displaying_left: SpeakerData = null
+var displaying_right: SpeakerData = null
 func display_dialogue(dialogue: DialogueData):
+	
+	if (dialogue.speaker1 == null or dialogue.speaker1 != displaying_left):
+		play_animation(true, false)
+	if (dialogue.speaker2 == null or dialogue.speaker2 != displaying_right):
+		play_animation(false, false)
+	if (dialogue.speaker1 != null and dialogue.speaker1 != displaying_left):
+		play_animation(true, true)
+	if (dialogue.speaker1 != null and dialogue.speaker1 != displaying_left):
+		play_animation(false, true)
+	displaying_left = dialogue.speaker1
+	displaying_right = dialogue.speaker2
+	
+	if (dialogue.speaker1):
+		update_speaker(speaker_box_1, dialogue.speaker1)
+	if (dialogue.speaker2):
+		update_speaker(speaker_box_2, dialogue.speaker2)
+
+	await wait_for_animations()
+	
+	
 	# cancelling mechanism
 	allowed_to_continue = false
 	allowed_to_skip_finish = false
--- ui/dialogue/speaker_data.gd
+++ ui/dialogue/speaker_data.gd
@@ -3,3 +3,12 @@ extends Resource
 
 @export var display_name: String = "Speaker"
 @export var sprite: Texture2D
+
+func is_equal(other: Variant) -> bool:
+	if self == other:
+		return true
+	if not other is SpeakerData:
+		return false
+	if other.display_name == display_name:
+		return true
+	return false
