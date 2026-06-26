ca39d61b025c3578b634e7f99086f19dbc81e4f5
Isaac Ng Jun Jie <143304834+IzN432@users.noreply.github.com>
2026-06-26 20:40:08 +0800

    Add menu (#28)

---
+++ menu/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var start_button = $VBoxContainer/Start
+@onready var options_button = $VBoxContainer/Options
+@onready var quit_button = $VBoxContainer/Quit
+
+func _ready():
+	start_button.pressed.connect(on_start)
+	options_button.pressed.connect(on_options)
+	if OS.has_feature("web"):
+		quit_button.hide()
+	quit_button.pressed.connect(on_quit)
+func on_start():
+	get_tree().change_scene_to_file("res://main/main.tscn")
+@onready var options_menu = $OptionsMenu
+func on_options():
+	options_menu.toggle()
+func on_quit():
+	get_tree().quit()
