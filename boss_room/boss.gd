a7e351f0f1ed58fea8558f9b6badf25edfe33e8a
yottajoules <169011335+yottajoules@users.noreply.github.com>
2026-06-27 21:09:04 +0800

    changed boss timer
    boss timer from 1 to 1.5

--- boss_room/boss.gd
+++ boss_room/boss.gd
@@ -15,7 +15,7 @@ func on_boss_death(enemy):
 @onready var meteor = preload("res://boss_room/meteor.tscn")
 func on_timer_timeout():
 	for i in 3:
-		await get_tree().create_timer(1, true, false ,false).timeout
+		await get_tree().create_timer(1.5, true, false ,false).timeout
 		var m = meteor.instantiate()
 		get_parent().add_child(m)
 		m.global_position = get_tree().get_first_node_in_group("Player").global_position
