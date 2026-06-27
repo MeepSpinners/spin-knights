499106f148043fa6c74ea159bfbc98d3e19ce09e
yottajoules <169011335+yottajoules@users.noreply.github.com>
2026-06-27 14:26:46 +0800

    added credits
    credit scene added

--- menu/credits_label.gd
+++ menu/credits_label.gd
@@ -1,5 +1,7 @@
-extends Control
+extends RichTextLabel
 
+@onready var wait_time = 1.5
+@onready var scroll_speed = 100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +10,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if wait_time > 0:
+		wait_time -= delta
+	else:	
+		var scrollbar = get_v_scroll_bar()
+		scrollbar.value += scroll_speed * delta
 	pass
